b21b818e8cf7bd72a1cb812f91a1512ada0d12fa
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-08-05 12:13:12 +0000

    Add copyright/license header.

--- replbot.js
+++ replbot.js
@@ -1,6 +1,24 @@
 #!/usr/bin/node
 'use strict';
 
+/*
+ * REPL Bot is a Discord bot that acts as a frontend to read-eval-print loop shells.
+ * Copyright (C) 2017  Jeffrey Thomas Piercy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const TOKEN = 'MzQzMzA2OTYyMjk5MjU2ODMz.DGcTVg.aCwgMyt1BvKgaViT-O_ZxeXoh2c';
 
 const Discord = require('discord.js');
